71476c79eef26d1d36eabaeac86b93733850558d
Caleb Buahin <calebgh@gmail.com>
2018-07-21 14:10:56 -0600

    fixed license error

--- include/hydrocouplespatialwkb.h
+++ include/hydrocouplespatialwkb.h
@@ -7,7 +7,7 @@
  * \license
  * This file and its associated files, and libraries are free software.
  * You can redistribute it and/or modify it under the terms of the
- * Lesser GNU General Public License as published by the Free Software Foundation;
+ * Lesser GNU Lesser General Public License as published by the Free Software Foundation;
  * either version 3 of the License, or (at your option) any later version.
  * This file and its associated files is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.(see <http://www.gnu.org/licenses/> for details)
